387cc942b51eb40e038f93acc67e5da34843b686
Tươi Lê Văn <tuoith95@gmail.com>
2022-11-05 16:58:15 +0700

    #2 file program update

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -2,7 +2,13 @@
 
 namespace OOP
 {
-    internal class Program
+    /// <summary>
+    /// 1 - Abstraction
+    /// 2 - Encapsulation
+    /// 3 - Inheritance
+    /// 4 - polymorphism
+    /// </summary>
+    public class Program
     {
         static void Main(string[] args)
         {
