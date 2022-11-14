dfaaee8355e9af2ce4b0b1fad7ea847b1730ab91
Tươi Lê Văn <tuoith95@gmail.com>
2022-11-14 13:48:45 +0700

    add extionsion method

--- OOP/Delegates/ClassA.cs
+++ OOP/Delegates/ClassA.cs
@@ -4,18 +4,26 @@ using System.Text;
 
 namespace OOP.Delegates
 {
-	// target function phải match với delegate (void MyDelegate(string msg) <=> void MethodA(string message))
+	/// <summary>
+	/// target function phải match với delegate (void MyDelegate(string msg) <=> void MethodA(string message))
+	/// 1. Declare a delegate
+	/// 2. Set a target method
+	/// 3. Invoke a delegate
+	/// </summary>
 	internal class ClassA
 	{
 		public delegate void MyDelegate(string msg);
 
 		// set target method
-		MyDelegate myDelegate = new MyDelegate(MethodA);
+		// MyDelegate myDelegate = new MyDelegate(MethodA);
 		// or
-		//MyDelegate myDelegate1 = MethodA;
+		// MyDelegate myDelegate1 = MethodA;
 		// or
-		//MyDelegate myDelegate2 = (string msg) => Console.WriteLine(msg);
-
+		// MyDelegate myDelegate2 = (string msg) => Console.WriteLine(msg);
+		/// <summary>
+		/// Truyền 1 hàm như 1 method
+		/// </summary>
+		/// <param name="myDelegate"></param>
 		public static void InvokeDelegate(MyDelegate myDelegate)
 		{
 			myDelegate("A method can have a parameter of the delegate type.");
