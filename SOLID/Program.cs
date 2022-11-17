0cc9a58a747e85ba7affc769867cdae39c6f850c
Tươi Lê Văn <tuoith95@gmail.com>
2022-11-17 10:10:04 +0700

    commit git source control

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -2,6 +2,15 @@
 
 namespace SOLID
 {
+	/// <summary>
+	/// Design principles provide high level guidelines to design better software applications.
+	/// They do not provide implementation guidelines and are not bound to any programming language.
+	/// The SOLID (SRP, OCP, LSP, ISP, DIP) principles are one of the most popular sets of design principles.
+
+	/// For example, the Single Responsibility Principle(SRP) suggests that a class should have only one reason to change.
+	/// This is a high-level statement which we can keep in mind while designing or creating classes for our application.
+	/// SRP does not provide specific implementation steps but it's up to you how you implement SRP in your application.
+	/// </summary>
 	public class Program
 	{
 		public void Test()
